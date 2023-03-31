78e0bb4ab768ee9ca388a5e5082d54523fe1dc07
KarthickKumar0408 <129404997+KarthickKumar0408@users.noreply.github.com>
2023-03-31 15:40:48 +0530

    Delete MS1_POSGRESQL directory

--- MS1_POSGRESQL/src/main/java/com/example/ms1/repo/ElementRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.ms1.repo;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.ms1.model.Element;
-
-public interface ElementRepo extends CrudRepository<Element, Integer> {
-
-}
